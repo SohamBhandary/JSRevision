430fbfbf003d9ba613c668e6dc9bd63eabb5a468
Soham Bhandary <bhandarysoham27@gmail.com>
2025-02-28 11:57:45 +0530

    Initial Commit

---
+++ Lecture4/Script.js
@@ -0,0 +1,32 @@
+// functions
+function abc(){
+    console.log("hey")
+}
+abc();
+function print(val)//->>PAramater
+{
+    
+}
+print(val)// ->>Argument
+
+function abcd()
+{
+    //function statemnet
+}
+var abcd=function(){
+    //function expression
+}
+// function(abcd){}
+
+    //anonymous function
+
+//fat arrow
+var a =()=>{
+
+}
+
+// Returns
+function abcd(){
+    return 12;
+}
+var a=abcd();
