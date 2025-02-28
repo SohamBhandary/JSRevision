a448b7fd66543cb67dd7788fb28e4be87f2480fa
Soham Bhandary <bhandarysoham27@gmail.com>
2025-02-28 11:38:24 +0530

    Initail Commit

---
+++ Lecture 3/script.js
@@ -0,0 +1,40 @@
+//types = primirtive datatype
+//refernce datatype
+// 12,12.4string.true,null,undefined are primitive
+// [] (){} are reference
+// var a
+// var a=12;
+// var a =true;
+// var a=[]
+// var a=false
+// var a =null
+// var a =function (){}
+// var arr=[1,2,3,4,5];
+// var ba=[...ar]
+// var age=18;
+// if(12>13){
+//     console.log("Hello")
+
+// }
+// else{
+//     console.log("Hey")
+// }
+/* if
+else if
+else if
+else*/
+// 1 to 10 loop
+for(var i=1;i<11;i++){
+
+}
+var i=1;
+while(i<11)
+{
+    i++;
+}
+
+//20 to 5 loop
+for(var i=20;i>4;i--)
+{
+
+}
