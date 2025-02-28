b205e63fb629339b96ff21526d602028df999e7b
Soham Bhandary <bhandarysoham27@gmail.com>
2025-02-28 15:39:11 +0530

    Initial Commit

---
+++ Lecture5/Script.js
@@ -0,0 +1,31 @@
+// Arrays
+// var users=["Soham","Soham1","Soham2","Soham3"];
+// console.log(users[0]);
+// var arr=[1,2,3,4]
+// for(var i=0;i<=arr.length;i++)
+// {
+//     console.log(arr[i]);
+
+// }
+//objects
+// var obj={}
+// var obj2=new Object();
+// var obj={
+//     name:"harsh",
+//     age:25,
+//     email:"kuchkuch@gmail.com",
+//     contact:123456789
+// };
+// console.log(obj.name)
+// console.log(obj)
+//Synchronus and asynchronus js
+// Synchonous code meih code humesha line by line chalta hai
+//async code line by line nahi chalta,saara async code ek saath shuru kar diya jaaata hai and jo pahle complete hojaaye uska answer de diya jaata hai
+
+
+//indepth
+
+
+
+
+
